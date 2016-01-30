8a409537223f68473d9d5eaedf81eaaaf9d5f771
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-01-30 19:34:58 -0200

    #449 Add Fish.how_to_configure() to functional tests

--- tests/functional/test_fish.py
+++ tests/functional/test_fish.py
@@ -1,6 +1,6 @@
 import pytest
 from tests.functional.plots import with_confirmation, without_confirmation, \
-    refuse_with_confirmation, select_command_with_arrows
+    refuse_with_confirmation, select_command_with_arrows, how_to_configure
 
 containers = (('thefuck/python3-fish',
                u'''FROM python:3
@@ -48,5 +48,11 @@ def test_refuse_with_confirmation(proc, TIMEOUT):
 @pytest.mark.functional
 def test_without_confirmation(proc, TIMEOUT):
     without_confirmation(proc, TIMEOUT)
+
+
+@pytest.mark.functional
+@pytest.mark.skip_without_docker
+def test_how_to_configure_alias(proc, TIMEOUT):
+    how_to_configure(proc, TIMEOUT)
 
 # TODO: ensure that history changes.
